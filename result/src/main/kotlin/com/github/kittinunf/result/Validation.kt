71630f903f00a4a9cd2ecaa648aebf22a1d8351e
loicdescotte <gitnexuspad1>
2016-07-02 09:11:40 +0200

    rename hasFailures, reformat code

--- result/src/main/kotlin/com/github/kittinunf/result/Validation.kt
+++ result/src/main/kotlin/com/github/kittinunf/result/Validation.kt
@@ -1,9 +1,9 @@
 package com.github.kittinunf.result
 
-class Validation<out E: Exception>(vararg val resultSequence: Result<*, E>) {
+class Validation<out E : Exception>(vararg val resultSequence: Result<*, E>) {
 
-    val failures: List<E> = resultSequence.filterIsInstance<Result.Failure<*, E>>().map{it.getException()}
+    val failures: List<E> = resultSequence.filterIsInstance<Result.Failure<*, E>>().map { it.getException() }
 
-    val hasFailures = failures.isNotEmpty()
+    val hasFailure = failures.isNotEmpty()
 
 }
--- result/src/test/kotlin/com/github/kittinunf/result/ValidationTests.kt
+++ result/src/test/kotlin/com/github/kittinunf/result/ValidationTests.kt
@@ -13,7 +13,7 @@ class ValidationTests {
         val r3: Result<Int, Exception> = Result.of(3)
 
         val validation = Validation(r1, r2, r3)
-        assertThat("validation.hasFailures", validation.hasFailures, isEqualTo(false))
+        assertThat("validation.hasFailures", validation.hasFailure, isEqualTo(false))
         assertThat("validation.failures", validation.failures, isEqualTo(listOf<Exception>()))
     }
 
@@ -21,12 +21,12 @@ class ValidationTests {
     fun validationTestWithError() {
 
         val r1: Result<Int, Exception> = Result.of(1)
-        val r2: Result<Int, Exception> = Result.of{throw Exception("Not a number")}
+        val r2: Result<Int, Exception> = Result.of { throw Exception("Not a number") }
         val r3: Result<Int, Exception> = Result.of(3)
-        val r4: Result<Int, Exception> = Result.of{throw Exception("Division by zero")}
+        val r4: Result<Int, Exception> = Result.of { throw Exception("Division by zero") }
 
         val validation = Validation(r1, r2, r3, r4)
-        assertThat("validation.hasFailures", validation.hasFailures, isEqualTo(true))
-        assertThat("validation.failures", validation.failures.map{it.message}, isEqualTo(listOf<String?>("Not a number", "Division by zero")))
+        assertThat("validation.hasFailures", validation.hasFailure, isEqualTo(true))
+        assertThat("validation.failures", validation.failures.map { it.message }, isEqualTo(listOf<String?>("Not a number", "Division by zero")))
     }
 }
