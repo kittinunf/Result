7e8152c3078c36baf5d7be94eee4f60827a5565e
Kittinun Vantasin <kittinun.f@gmail.com>
2016-10-15 19:52:43 +0900

    Update to version 1.0.4 (#17)

--- result/src/main/kotlin/com/github/kittinunf/result/Validation.kt
+++ result/src/main/kotlin/com/github/kittinunf/result/Validation.kt
@@ -1,6 +1,6 @@
 package com.github.kittinunf.result
 
-class Validation<out E : Exception>(vararg val resultSequence: Result<*, E>) {
+class Validation<out E : Exception>(vararg resultSequence: Result<*, E>) {
 
     val failures: List<E> = resultSequence.filterIsInstance<Result.Failure<*, E>>().map { it.getException() }
 
