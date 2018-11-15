b55a36df182b27b1cbd93b8a0babb3e0fddfc1d1
Kittinun Vantasin <kittinun.f@gmail.com>
2018-11-15 10:48:32 +0900

    WIP

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
+
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", "1.3.10"))
+    }
+}
+
+allprojects {
+    repositories {
+        jcenter()
+    }
+}
+
+subprojects {
+    apply {
+        plugin<JavaLibraryPlugin>()
+        plugin<KotlinPlatformJvmPlugin>()
+    }
+}
