3c91c7fdd511426785b92d76b5dd655a126adb3c
Kittinun Vantasin <kittinun.f@gmail.com>
2018-11-18 00:09:09 +0900

    Update publishing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
 
 buildscript {
@@ -27,6 +28,58 @@ subprojects {
         plugin("jacoco")
     }
 
+    // publishing
+    val sourceSets = project.the<SourceSetContainer>()
+
+    val sourcesJar by tasks.registering(Jar::class) {
+        from(sourceSets["main"].allSource)
+        classifier = "sources"
+    }
+
+    val javadocJar by tasks.creating(Jar::class) {
+        from(sourceSets["main"].allJava)
+        classifier = "javadoc"
+    }
+
+    val artifactPublish: String by extra
+    val artifactGroupId: String by extra
+
+    version = artifactPublish
+    group = artifactGroupId
+
+    configure<PublishingExtension> {
+        publications {
+            register(project.name, MavenPublication::class) {
+                from(components["java"])
+                artifact(sourcesJar.get())
+                artifact(javadocJar)
+                groupId = artifactGroupId
+                artifactId = project.name
+                version = artifactPublish
+            }
+        }
+    }
+
+    // bintray
+    configure<BintrayExtension> {
+        user = findProperty("BINTRAY_USER") as? String
+        key = findProperty("BINTRAY_KEY") as? String
+        setPublications(project.name)
+        pkg.apply {
+            repo = "maven"
+            name = "Result"
+            desc = "The modelling for success/failure of operations in Kotlin"
+            userOrg = "kittinunf"
+            websiteUrl = "https://github.com/kittinunf/Result"
+            vcsUrl = "https://github.com/kittinunf/Result"
+            setLicenses("MIT")
+            version.apply {
+                name = artifactPublish
+            }
+        }
+    }
+
+    // jacoco
     configure<JacocoPluginExtension> {
         toolVersion = extra.get("jacoco") as String
     }
