a15a728b5bbdc03e36ecd0c325b336fedb8176de
Markus Padourek <markus.padourek@gmail.com>
2020-01-31 12:38:04 +0100

    Allow result to have a nullable type

--- result-coroutines/src/main/kotlin/com/github/kittinunf/result/coroutines/SuspendableResult.kt
+++ result-coroutines/src/main/kotlin/com/github/kittinunf/result/coroutines/SuspendableResult.kt
@@ -5,21 +5,21 @@ inline fun <reified X> SuspendableResult<*, *>.getAs() = when (this) {
     is SuspendableResult.Failure -> error as? X
 }
 
-suspend fun <V : Any> SuspendableResult<V, *>.success(f: suspend (V) -> Unit) = fold(f, {})
+suspend fun <V : Any?> SuspendableResult<V, *>.success(f: suspend (V) -> Unit) = fold(f, {})
 
 suspend fun <E : Exception> SuspendableResult<*, E>.failure(f: suspend (E) -> Unit) = fold({}, f)
 
-infix fun <V : Any, E : Exception> SuspendableResult<V, E>.or(fallback: V) = when (this) {
+infix fun <V : Any?, E : Exception> SuspendableResult<V, E>.or(fallback: V) = when (this) {
     is SuspendableResult.Success -> this
     else -> SuspendableResult.Success(fallback)
 }
 
-infix fun <V : Any, E : Exception> SuspendableResult<V, E>.getOrElse(fallback: V) = when (this) {
+infix fun <V : Any?, E : Exception> SuspendableResult<V, E>.getOrElse(fallback: V) = when (this) {
     is SuspendableResult.Success -> value
     else -> fallback
 }
 
-suspend fun <V : Any, U : Any, E : Exception> SuspendableResult<V, E>.map(transform: suspend (V) -> U): SuspendableResult<U, E> = try {
+suspend fun <V : Any?, U : Any?, E : Exception> SuspendableResult<V, E>.map(transform: suspend (V) -> U): SuspendableResult<U, E> = try {
     when (this) {
         is SuspendableResult.Success -> SuspendableResult.Success(transform(value))
         is SuspendableResult.Failure -> SuspendableResult.Failure(error)
@@ -28,7 +28,7 @@ suspend fun <V : Any, U : Any, E : Exception> SuspendableResult<V, E>.map(transf
     SuspendableResult.error(ex as E)
 }
 
-suspend fun <V : Any, U : Any, E : Exception> SuspendableResult<V, E>.flatMap(transform: suspend (V) -> SuspendableResult<U, E>): SuspendableResult<U, E> = try {
+suspend fun <V : Any?, U : Any?, E : Exception> SuspendableResult<V, E>.flatMap(transform: suspend (V) -> SuspendableResult<U, E>): SuspendableResult<U, E> = try {
     when (this) {
         is SuspendableResult.Success -> transform(value)
         is SuspendableResult.Failure -> SuspendableResult.Failure(error)
@@ -37,17 +37,17 @@ suspend fun <V : Any, U : Any, E : Exception> SuspendableResult<V, E>.flatMap(tr
     SuspendableResult.error(ex as E)
 }
 
-suspend fun <V : Any, E : Exception, E2 : Exception> SuspendableResult<V, E>.mapError(transform: suspend (E) -> E2) = when (this) {
+suspend fun <V : Any?, E : Exception, E2 : Exception> SuspendableResult<V, E>.mapError(transform: suspend (E) -> E2) = when (this) {
     is SuspendableResult.Success -> SuspendableResult.Success<V, E2>(value)
     is SuspendableResult.Failure -> SuspendableResult.Failure<V, E2>(transform(error))
 }
 
-suspend fun <V : Any, E : Exception, E2 : Exception> SuspendableResult<V, E>.flatMapError(transform: suspend (E) -> SuspendableResult<V, E2>) = when (this) {
+suspend fun <V : Any?, E : Exception, E2 : Exception> SuspendableResult<V, E>.flatMapError(transform: suspend (E) -> SuspendableResult<V, E2>) = when (this) {
     is SuspendableResult.Success -> SuspendableResult.Success(value)
     is SuspendableResult.Failure -> transform(error)
 }
 
-suspend fun <V : Any, E : Exception> SuspendableResult<V, E>.any(predicate: suspend (V) -> Boolean): Boolean = try {
+suspend fun <V : Any?, E : Exception> SuspendableResult<V, E>.any(predicate: suspend (V) -> Boolean): Boolean = try {
     when (this) {
         is SuspendableResult.Success -> predicate(value)
         is SuspendableResult.Failure -> false
@@ -56,17 +56,17 @@ suspend fun <V : Any, E : Exception> SuspendableResult<V, E>.any(predicate: susp
     false
 }
 
-suspend fun <V : Any, U: Any> SuspendableResult<V, *>.fanout(other: suspend () -> SuspendableResult<U, *>): SuspendableResult<Pair<V, U>, *> =
+suspend fun <V : Any?, U: Any> SuspendableResult<V, *>.fanout(other: suspend () -> SuspendableResult<U, *>): SuspendableResult<Pair<V, U>, *> =
     flatMap { outer -> other().map { outer to it } }
 
 
-suspend fun <V : Any, E : Exception> List<SuspendableResult<V, E>>.lift(): SuspendableResult<List<V>, E> = fold(SuspendableResult.Success<MutableList<V>, E>(mutableListOf<V>()) as SuspendableResult<MutableList<V>, E>) { acc, result ->
+suspend fun <V : Any?, E : Exception> List<SuspendableResult<V, E>>.lift(): SuspendableResult<List<V>, E> = fold(SuspendableResult.Success<MutableList<V>, E>(mutableListOf<V>()) as SuspendableResult<MutableList<V>, E>) { acc, result ->
     acc.flatMap { combine ->
         result.map { combine.apply { add(it) } }
     }
 }
 
-sealed class SuspendableResult<out V : Any, out E : Exception> {
+sealed class SuspendableResult<out V : Any?, out E : Exception> {
 
     abstract operator fun component1(): V?
     abstract operator fun component2(): E?
@@ -80,7 +80,7 @@ sealed class SuspendableResult<out V : Any, out E : Exception> {
 
     abstract fun get(): V
 
-    class Success<out V : Any, out E : Exception>(val value: V) : SuspendableResult<V, E>() {
+    class Success<out V : Any?, out E : Exception>(val value: V) : SuspendableResult<V, E>() {
         override fun component1(): V? = value
         override fun component2(): E? = null
 
@@ -96,7 +96,7 @@ sealed class SuspendableResult<out V : Any, out E : Exception> {
         }
     }
 
-    class Failure<out V : Any, out E : Exception>(val error: E) : SuspendableResult<V, E>() {
+    class Failure<out V : Any?, out E : Exception>(val error: E) : SuspendableResult<V, E>() {
         override fun component1(): V? = null
         override fun component2(): E? = error
 
@@ -118,11 +118,11 @@ sealed class SuspendableResult<out V : Any, out E : Exception> {
         // Factory methods
         fun <E : Exception> error(ex: E) = Failure<Nothing, E>(ex)
 
-        fun <V : Any> of(value: V?, fail: (() -> Exception) = { Exception() }): SuspendableResult<V, Exception> {
+        fun <V : Any?> of(value: V?, fail: (() -> Exception) = { Exception() }): SuspendableResult<V, Exception> {
             return value?.let { Success<V, Nothing>(it) } ?: error(fail())
         }
 
-        suspend fun <V : Any, E: Exception> of(f: suspend () -> V): SuspendableResult<V, E> = try {
+        suspend fun <V : Any?, E: Exception> of(f: suspend () -> V): SuspendableResult<V, E> = try {
             Success(f())
         } catch(ex: Exception) {
             Failure(ex as E)
