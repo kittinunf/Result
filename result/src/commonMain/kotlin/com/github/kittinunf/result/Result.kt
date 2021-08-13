1a795fa63313fceeae910b9d2be2276ecba1c149
Felipe Lopes <felipe.lopes@alice.com.br>
2021-08-13 11:25:47 -0300

    Fix formatting

--- result/src/commonMain/kotlin/com/github/kittinunf/result/Result.kt
+++ result/src/commonMain/kotlin/com/github/kittinunf/result/Result.kt
@@ -3,9 +3,9 @@ package com.github.kittinunf.result
 import com.github.kittinunf.result.Kind.Failure
 import com.github.kittinunf.result.Kind.Success
 
-fun<V: Any> V.success(): Result.Success<V> = Result.success(this)
+fun <V: Any> V.success(): Result.Success<V> = Result.success(this)
 
-fun<E: Throwable> E.failure(): Result.Failure<E> = Result.failure(this)
+fun <E: Throwable> E.failure(): Result.Failure<E> = Result.failure(this)
 
 inline fun <V> Result<V, *>.success(f: (V) -> Unit) = fold(f, {})
 
