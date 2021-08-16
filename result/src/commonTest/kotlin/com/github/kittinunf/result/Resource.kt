48065b962c941e2aada04406e73a8c674c2a6b8e
Kittinun Vantasin <kittinun.f@gmail.com>
2021-08-16 11:40:33 +0900

    Revert as it has nothing to do with path

--- result/src/commonTest/kotlin/com/github/kittinunf/result/Resource.kt
+++ result/src/commonTest/kotlin/com/github/kittinunf/result/Resource.kt
@@ -1,6 +1,6 @@
 package com.github.kittinunf.result
 
-const val resource = "../../../../result/src/commonTest/resources"
+const val resource = "./src/commonTest/resources"
 
 expect class Resource(name: String) {
     val name: String
