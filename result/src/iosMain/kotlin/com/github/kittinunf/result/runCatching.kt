c3edf8dffa9ee6c5db1bdeafd2af7d5477e044a9
iNoles <iNoles@users.noreply.github.com>
2021-08-18 00:24:02 -0400

    Expect RunCatching for JS,Native and JVM for proper catch.

---
+++ result/src/iosMain/kotlin/com/github/kittinunf/result/runCatching.kt
@@ -0,0 +1,17 @@
+package com.github.kittinunf.result
+
+actual inline fun <V> runCatching(block: () -> V?): Result<V?, Throwable> {
+    return try {
+        Result.success(block())
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+}
+
+actual inline infix fun <T, V> T.runCatching(block: T.() -> V?): Result<V?, Throwable> {
+    return try {
+        Result.success(block())
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+}
