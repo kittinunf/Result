e90dec581e607e850bd6cf4122a7efdfa889f751
Kittinun Vantasin <kittinun.f@gmail.com>
2023-03-30 21:32:47 +0900

    Update version for 1.8.0

--- result/build.gradle.kts
+++ result/build.gradle.kts
@@ -23,7 +23,7 @@ version = if (isReleaseBuild) artifactPublishVersion else "master-$gitSha-SNAPSH
 kotlin {
     jvm()
     ios()
-    js {
+    js(LEGACY) {
         nodejs()
         binaries.executable()
     }
