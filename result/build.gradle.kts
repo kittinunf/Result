d37c5a7bd5e8110bcb30bd07ef2924ce10939da9
Jonathan Steele <iNoles@users.noreply.github.com>
2023-07-16 19:42:45 -0400

    Upgrade Kotlin to 1.9.0

--- result/build.gradle.kts
+++ result/build.gradle.kts
@@ -22,8 +22,7 @@ version = if (isReleaseBuild) artifactPublishVersion else "master-$gitSha-SNAPSH
 
 kotlin {
     jvm()
-    iosX64()
-    iosArm64()
+    ios()
     js(IR) {
         browser()
         binaries.executable()
@@ -35,17 +34,6 @@ kotlin {
     iosSimulatorArm64()
     macosArm64()
     macosX64()
-
-    // Apply the default hierarchy again. It'll create, for example, the iosMain source set:
-    applyDefaultHierarchyTemplate()
-
-    targets.configureEach {
-        compilations.configureEach {
-            compilerOptions.configure {
-                freeCompilerArgs.add("-Xexpect-actual-classes")
-            }
-        }
-    }
 
     sourceSets {
         val commonMain by getting
@@ -68,6 +56,39 @@ kotlin {
             dependencies {
                 implementation(libs.kotlin.test.js)
             }
+        }
+
+        val appleMain by creating {
+            dependsOn(commonMain)
+        }
+        val appleTest by creating {
+            dependsOn(commonTest)
+        }
+
+        val iosMain by getting {
+            dependsOn(appleMain)
+        }
+        val macosArm64Main by getting {
+            dependsOn(appleMain)
+        }
+        val macosX64Main by getting {
+            dependsOn(appleMain)
+        }
+        val iosSimulatorArm64Main by getting {
+            dependsOn(appleMain)
+        }
+
+        val iosTest by getting {
+            dependsOn(appleTest)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependsOn(appleTest)
+        }
+        val macosX64Test by getting {
+            dependsOn(appleTest)
+        }
+        val macosArm64Test by getting {
+            dependsOn(appleTest)
         }
     }
 }
