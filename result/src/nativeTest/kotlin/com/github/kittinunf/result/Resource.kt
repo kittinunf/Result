27e84efd00fdb93ff33361c95a457d5f33326225
Jonathan Steele <iNoles@users.noreply.github.com>
2023-12-22 23:04:45 -0500

    trying to fix a lot of warnings by super linter

--- result/src/nativeTest/kotlin/com/github/kittinunf/result/Resource.kt
+++ result/src/nativeTest/kotlin/com/github/kittinunf/result/Resource.kt
@@ -7,16 +7,14 @@ actual class Resource actual constructor(actual val name: String) {
 
     actual fun read(): String {
         return when {
-            name.contains("lorem_short") -> {
-                """
+            name.contains("lorem_short") -> """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       """.trimIndent()
-            }
-            name.contains("lorem_long") -> {
-                """
+
+            name.contains("lorem_long") -> """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mollis eleifend libero, id elementum sem sagittis sit amet.
 Donec lorem nulla, mollis eu diam eget, varius facilisis ipsum. Duis turpis lorem, iaculis quis venenatis nec, placerat eget est.
 Nullam posuere leo sit amet diam laoreet finibus. Suspendisse at tempor nibh, sit amet maximus elit.
@@ -33,7 +31,7 @@ Vestibulum commodo nibh ex, at porttitor mi tristique in. Donec bibendum, sapien
 Proin elementum libero in viverra dignissim. Morbi commodo eget tellus a congue. Suspendisse potenti. Interdum et malesuada fames ac ante ipsum primis in faucibus.
 Praesent euismod aliquet ligula, et consequat tortor sodales feugiat. Praesent et dolor felis.
         """.trimIndent()
-            }
+
             else -> throw RuntimeException("src/commonTest/resources/$name (No such file or directory)")
         }
     }
